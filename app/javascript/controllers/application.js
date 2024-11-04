aa004849153eaf49a3b0af0ea6beaebb5a269e8f
moyac1012 <moyac@realidea.jp>
2024-11-04 17:43:02 +0900

    Enterキー発火を防ぐ

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,18 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('new-post-form');
+  
+    if (form) {
+      form.addEventListener('keydown', function(event) {
+        // Enterキーが押されたときに送信を防ぐ
+        if (event.key === 'Enter') {
+          event.preventDefault();
+          return false;
+        }
+      });
+    }
+  });
+
 export { application }
